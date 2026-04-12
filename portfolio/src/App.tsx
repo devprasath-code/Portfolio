f9466e8d5950bb654c010ad64a4b7afadd537eb2
Dev Prasath <devprasathloganathan@gmail.com>
2026-04-12 22:04:36 +0530

    revert: remove AI assistant and custom cursor

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -13,8 +13,6 @@ import { Scene } from './components/Scene';
 import { Navigation } from './components/Navigation';
 
 import { Overlay } from './components/Overlay';
-import { AICursor } from './components/AICursor';
-import { AIAssistant } from './components/AIAssistant';
 import { motion, AnimatePresence } from 'framer-motion';
 import Lenis from 'lenis';
 
@@ -71,8 +69,6 @@ export default function App() {
       <Scene />
       <Overlay />
       <Navigation />
-      <AICursor />
-      <AIAssistant />
     </main>
   );
 }
